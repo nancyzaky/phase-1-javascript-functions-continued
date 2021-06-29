f54ea8c8a989a218f95e65ca79687725945a17e1
Nancy Zaky <nancyzki@yahoo.com>
2021-06-28 17:46:54 -0700

    commit tree 206adf5ec04a2ec27ee9414a48b3954c17e6deef

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code here
+const saturdayFun = (activity= "roller-skate") => {
+  return `This Saturday, I want to ${activity}!`
+}
+
+
+const mondayWork = (activity = 'go to the office') => {
+  return `This Monday, I will ${activity}.`
+}
+
+const wrapAdjective = (flair="*") => {
+  return (param = 'special') => {
+    return `You are ${flair}${param}${flair}!`
+  }
+}
